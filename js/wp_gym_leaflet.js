af2e8db22e2699a899abeb31619340b4b224f770
Eric Whitcomb <eric.whitcomb@gmail.com>
2021-08-24 23:30:03 -0600

    Add the map code to our javascript
    We use the hidden inputs from the plugin to populate values in the map code

--- js/wp_gym_leaflet.js
+++ js/wp_gym_leaflet.js
@@ -1,1 +1,19 @@
-console.log("location script engaged");
+document.addEventListener("DOMContentLoaded", () => {
+  // Read from the hidden inputs
+  const lat = document.querySelector("#lat").value;
+  const lng = document.querySelector("#lng").value;
+  const zoom = document.querySelector("#zoom").value;
+  const address = document.querySelector("#address").value;
+
+  if (lat && lng) {
+    // Add them into the map
+    let map = L.map("map").setView([lat, lng], zoom);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([lat, lng]).addTo(map).bindPopup(address).openPopup();
+  }
+});
